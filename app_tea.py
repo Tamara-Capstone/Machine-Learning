e9e163f078a3feda91f762aa6e5f2e7dc415439b
yeremiapane <101639911+yeremiapane@users.noreply.github.com>
2023-06-02 15:51:33 +0700

    add each plant API

---
+++ app_tea.py
@@ -0,0 +1,87 @@
+from flask import Flask, request, jsonify
+from werkzeug.utils import secure_filename
+import numpy as np
+from keras.models import load_model
+from tensorflow.keras.preprocessing import image
+from keras.applications.imagenet_utils import preprocess_input
+
+app = Flask(__name__)
+
+# Load the trained model
+MODEL_PATH = 'Tea_models.h5'
+model = load_model(MODEL_PATH)
+model.make_predict_function()  # Necessary
+
+label_map = {
+    0: "Tea_algal_leaf",
+    1: "Tea_anthracnose",
+    2: "Tea_bird_eye_spot",
+    3: "Tea_brown_blight",
+    4: "Tea_healthy",
+    5: "Tea_red_leaf_spot"
+}
+
+def model_predict(img_path, model):
+    """
+    Predict the disease of a Tea leaf image.
+
+    Args:
+        img_path (str): The path to the image file.
+        model (keras.Model): The trained model.
+
+    Returns:
+        A tuple containing the predicted class index and its probability.
+    """
+    img = image.load_img(img_path, target_size=(224, 224))
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+
+    # Apply preprocessing (e.g., normalization)
+    x = preprocess_input(x)
+
+    preds = model.predict(x)
+    predicted_class_index = np.argmax(preds)
+    predicted_class_prob = preds[0][predicted_class_index]
+    return predicted_class_index, predicted_class_prob
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    """
+    Predict the disease of a Tea leaf image uploaded by the user.
+
+    Returns:
+        A JSON response containing the predicted disease label and its probability.
+    """
+    # Check if an image file was uploaded
+    if 'file' not in request.files:
+        return jsonify({'error': 'No file found'})
+
+    file = request.files['file']
+
+    # Check if the file is a valid image
+    if file.filename == '':
+        return jsonify({'error': 'No image file selected'})
+
+    if not file.filename.lower().endswith(('.jpg', '.jpeg', '.png', '.gif')):
+        return jsonify({'error': 'Invalid image file'})
+
+    # Save the file
+    file_path = secure_filename(file.filename)
+    file.save(file_path)
+
+    # Make prediction
+    predicted_class_index, predicted_class_prob = model_predict(file_path, model)
+
+    # Process the result
+    if predicted_class_prob < 0.3:
+        predicted_label = "Unclear"
+        predicted_prob = predicted_class_prob * 100
+    else:
+        predicted_label = label_map[predicted_class_index]
+        predicted_prob = predicted_class_prob * 100
+
+    # Return the result as JSON
+    return jsonify({'label': predicted_label, 'probability': round(predicted_prob, 2)})
+
+if __name__ == '__main__':
+    app.run(debug=True)
